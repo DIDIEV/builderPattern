51e538152ce5245e04faf24fb41f04880300142a
DIDIEV <stiventorres608@gmail.com>
2025-09-18 09:54:35 -0500

    Update MilkShake.java

---
+++ MilkShakes/MilkShake.java
@@ -0,0 +1,31 @@
+package MilkShakes;
+
+public class MilkShake {
+    private String name;
+    private String milkFlavour;
+    private String iceCreamFlavour;
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMilkFlavour() {
+        return milkFlavour;
+    }
+
+    public void setMilkFlavour(String milkFlavour) {
+        this.milkFlavour = milkFlavour;
+    }
+
+    public String getIceCreamFlavour() {
+        return iceCreamFlavour;
+    }
+
+    public void setIceCreamFlavour(String iceCreamFlavour) {
+        this.iceCreamFlavour = iceCreamFlavour;
+    }
+}
