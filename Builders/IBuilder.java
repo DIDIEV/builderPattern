fa136feeda20761e43482e9537909d84e97002b0
DIDIEV <stiventorres608@gmail.com>
2025-09-18 09:55:21 -0500

    Update IBuilder.java

--- Builders/IBuilder.java
+++ Builders/IBuilder.java
@@ -1,5 +1,10 @@
 package Builders;
 
+import MilkShakes.MilkShake;
+
 public interface IBuilder {
-    
+    public void buildName(String name);
+    public void buildMilk(String milkFlavour);
+    public void buildIceCream(String iceCreamFlavour);
+    public MilkShake getMilkShake();
 }
